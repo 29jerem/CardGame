fa6f6c7837d5cd1d490924ab63a605b5211b31b3
jeremy <jeremy.quetin@icloud.com>
2019-12-08 00:49:37 +0100

    9. Clean a little the code for Event

--- Card Game/Assets/Script/CardDisplay.cs
+++ Card Game/Assets/Script/CardDisplay.cs
@@ -4,24 +4,103 @@ using UnityEngine;
 
 public class CardDisplay : MonoBehaviour
 {
-    public int idCardDataBase;
-    public bool isBack;
+    public const string nameOfTheCard = "Name";
+    public const string descriptionOfTheCard = "Description";
+    public const string manaOfTheCard = "Mana";
+    public const string attackOfTheCard = "Attack";
+    public const string healthOfTheCard = "Health";
+    public const string artworkmaskOfTheCard = "ArtWorkMask";
+    public const string artworkOfTheCard = "ArtWork";
+    public const string backOfTheCard = "Back";
 
-    void Awake()
+    // Refresh the card with the id and if is the back of the card is present
+    public static void InitializeCard(int idCardDB, bool isBack, GameObject card)
     {
-        idCardDataBase = 0;
-        isBack = false;
+        // Give the information of the card
+        Card cardDataBase = CardDataBase.cardDB[idCardDB];
+        DisplayBack(card, isBack);
+        ChangeTextName(card, cardDataBase.cardName);
+        ChangeTextDescription(card, cardDataBase.description);
+        ChangeTextMana(card, cardDataBase.mana);
+        ChangeTextAttack(card, cardDataBase.attack);
+        ChangeTextHealth(card, cardDataBase.health);
+        ChangeArtWork(card, cardDataBase.artWork);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    // Set the text of name
+    public static bool ChangeTextName(GameObject card, string textToChange)
     {
-        
+        return ChangeText(card, textToChange, CardDisplay.nameOfTheCard);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Set the text of the description
+    public static bool ChangeTextDescription(GameObject card, string textToChange)
     {
-        
+        return ChangeText(card, textToChange, CardDisplay.descriptionOfTheCard);
+    }
+
+    // Set the text of mana
+    public static bool ChangeTextMana(GameObject card, int textToChange)
+    {
+        return ChangeText(card, textToChange.ToString(), CardDisplay.manaOfTheCard);
+    }
+
+    // Set the text of Attack
+    public static bool ChangeTextAttack(GameObject card, int textToChange)
+    {
+        return ChangeText(card, textToChange.ToString(), CardDisplay.attackOfTheCard);
+    }
+
+    // Set the text of Health
+    public static bool ChangeTextHealth(GameObject card, int textToChange)
+    {
+        return ChangeText(card, textToChange.ToString(), CardDisplay.healthOfTheCard);
+    }
+
+    // Set the text of a property of a card
+    public static bool ChangeText(GameObject card, string textToChange, string textDescription)
+    {
+        Transform textTransform = card.transform.Find(textDescription);
+        if(textTransform != null)
+        {
+            textTransform.gameObject.GetComponent<UnityEngine.UI.Text>().text = textToChange;
+            return true;
+        }
+        return false;
+    }
+
+    // Set the image of ArtWork
+    public static bool ChangeArtWork(GameObject card, Sprite artWork)
+    {
+        Transform artWorkMaskTransform= card.transform.Find(CardDisplay.artworkmaskOfTheCard);
+        if(artWorkMaskTransform != null)
+        {
+            Transform artWorkTransform = artWorkMaskTransform.Find(CardDisplay.artworkOfTheCard);
+            if(artWorkTransform  != null)
+            {
+                artWorkTransform.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = artWork;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Active or not the back of the card
+    public static bool DisplayBack(GameObject card, bool back)
+    {
+        Transform cardBack = card.transform.Find(CardDisplay.backOfTheCard);
+        if(cardBack != null)
+        {
+            if(back)
+            {
+                cardBack.gameObject.SetActive(true);
+            }
+            else
+            {
+                cardBack.gameObject.SetActive(false);
+            }
+            return true;
+        }
+        return false;
     }
 }
--- Card Game/Assets/Script/Event.cs
+++ Card Game/Assets/Script/Event.cs
@@ -20,35 +20,12 @@ public class Event : MonoBehaviour
             GameObject carte = Instantiate(cardPrefab) as GameObject;
 
             // Position the card
-            GameObject canvas = GameObject.Find("Canvas");
+            GameObject canvas = GameObject.Find("MyDeck");
             carte.transform.SetParent(canvas.transform);
             carte.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 
-            // Give an id which increase each time user create a card
-            //carte.GetComponent<CardDisplay>().idCardDataBase = increment;
-            Debug.Log("L'identifiant de la carte est : " + carte.GetComponent<CardDisplay>().idCardDataBase);
-
-            // Give the information of the card
-            Card cardDataBase = CardDataBase.cardDB[carte.GetComponent<CardDisplay>().idCardDataBase];
-            carte.transform.Find("Name").gameObject.GetComponent<UnityEngine.UI.Text>().text = cardDataBase.cardName;
-            carte.transform.Find("Description").gameObject.GetComponent<UnityEngine.UI.Text>().text = cardDataBase.description;
-            carte.transform.Find("Mana").gameObject.GetComponent<UnityEngine.UI.Text>().text = cardDataBase.mana.ToString();
-            carte.transform.Find("Health").gameObject.GetComponent<UnityEngine.UI.Text>().text = cardDataBase.health.ToString();
-            carte.transform.Find("Attack").gameObject.GetComponent<UnityEngine.UI.Text>().text = cardDataBase.attack.ToString();
-            carte.transform.Find("ArtWorkMask").gameObject.transform.Find("ArtWork").gameObject.GetComponent<UnityEngine.UI.Image>().sprite = cardDataBase.artWork;
-
-            //  Check if the card back is false
-            if (!carte.GetComponent<CardDisplay>().isBack)
-            {
-                // To disable the card back
-                GameObject cardBack = carte.transform.Find("Back").gameObject;
-                if(cardBack)
-                {
-                    cardBack.SetActive(false);
-                }
-            }
-            
-            
+            // Initialize the card
+            CardDisplay.InitializeCard(0, false, carte);            
         }
     }
 }
